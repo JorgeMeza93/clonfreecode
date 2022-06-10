8d81494afe47a8188014f24523223a99bb1b5fcb
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-10 00:23:01 -0500

    Utilizacion de componentes funcionales

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/testimonio.css"
+
+function Testimonio( props ){
+    return (
+        <div className="contenedor-testimonio">
+            <img className="imagen-testimonio" src={require("../imagenes/testimonio-emma.png")} alt="Fotografía de Emma"/>
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio">{ props.nombre } en { props.pais }</p>
+                <p className="cargo-testimonio">{ props.cargo } en { props.empresa}</p>
+                <p className="texto-testimonio">"{props.testimonio}"</p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Testimonio;

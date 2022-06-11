2d0cb8f80f36b935a1a1c5b9ecc8d5c6a388e0cf
Hayda Bello <haydabello@iMac-de-Hayda.local>
2022-06-10 23:23:43 -0500

    Proyecto finalizado

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -4,10 +4,10 @@ import "../styles/testimonio.css"
 function Testimonio( props ){
     return (
         <div className="contenedor-testimonio">
-            <img className="imagen-testimonio" src={require("../imagenes/testimonio-emma.png")} alt="Fotografía de Emma"/>
+            <img className="imagen-testimonio" src={ require(`../imagenes/${props.imagen}.png` ) } alt="Fotografía de Emma"/>
             <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio">{ props.nombre } en { props.pais }</p>
-                <p className="cargo-testimonio">{ props.cargo } en { props.empresa}</p>
+                <p className="nombre-testimonio"><strong>{ props.nombre }</strong> en { props.pais }</p>
+                <p className="cargo-testimonio">{ props.cargo } en <strong>{ props.empresa}</strong></p>
                 <p className="texto-testimonio">"{props.testimonio}"</p>
             </div>
         </div>
